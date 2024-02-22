f4b2c20ef97020fab29c7e214d7766c36e1f7a31
mrrobot911 <kelwin2705@gmail.com>
2024-02-22 18:10:02 +0300

    feat: dist path

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -26,7 +26,7 @@ const baseConfig = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, './dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
