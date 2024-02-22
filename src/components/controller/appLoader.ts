9e61e9a8b8f5e81946392a041d9f746c56d3d620
mrrobot911 <kelwin2705@gmail.com>
2024-02-22 18:33:48 +0300

    feat:proxy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://proxi-add-cors.vercel.app/?url=https://newsapi.org/v2/', {
       apiKey: '40db993a2f4144668d0904a64855aaaa',
     });
   }
